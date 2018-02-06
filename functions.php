60370c8dfaf55bbaa09a1fd455ac5f758b765c84
eyal670 <eyal670@gmail.com>
2018-02-06 10:40:45 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+//all media attachment pages redirect to... , good for SEO
+function myprefix_redirect_attachment_page() {
+	if ( is_attachment() ) {
+		global $post;
+		if ( $post && $post->post_parent ) {
+			wp_redirect( esc_url( get_permalink( $post->post_parent ) ), 301 );
+			exit;
+		} else {
+			wp_redirect( esc_url( home_url( '/' ) ), 301 );
+			exit;
+		}
+	}
+}
+add_action( 'template_redirect', 'myprefix_redirect_attachment_page' );
